be39f7f80f180d48d2e91fc8c74afb90cee842ac
nfinam161090 <nfinam161090@gmail.com>
2026-09-04 20:58:24 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // DÁN WEB APP URL CỦA GOOGLE APPS SCRIPT VÀO ĐÂY.
 // Ví dụ:
 // const API_URL = "https://script.google.com/macros/s/XXXXXXXX/exec";
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbxfTp4h1e_hHaNQdfvnkQ5r9AKyfJNGHA3F3UmqcBrzZTVYAgNB7zr6CfzgjxqzNRQYxQ/exec";
